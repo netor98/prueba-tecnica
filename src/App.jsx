808ee530ef5567d3ba279b5c9f17babfcc84ee78
napo <pcu18021379@gmail.com>
2025-08-26 10:54:32 -0700

    Renderizado datos de la api

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const API_URL = 'https://swapi.info/api'
+
+  const [characters, setCharacters] = useState([])
+
+  useEffect(() => {
+    fetch(`${API_URL}/people`)
+      .then((res) => res.json())
+      .then((json) => setCharacters(json))
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <table>
+        <thead>
+          <tr>
+            <td>Nombre</td>
+            <td>Altura</td>
+            <td>Peso</td>
+            <td>Color de cabello</td>
+            <td>Color de piel</td>
+            <td>Color de ojos</td>
+            <td>Fecha de nacimiento</td>
+            <td>Genero</td>
+            <td>Planeta de nacimiento</td>
+          </tr>
+        </thead>
+
+        <tbody>
+          {characters.map((character) => (
+            <tr key={character.id}>
+              <td>{character.name}</td>
+              <td>{character.height}</td>
+              <td>{character.mass}</td>
+              <td>{character.hair_color}</td>
+              <td>{character.skin_color}</td>
+              <td>{character.eye_color}</td>
+              <td>{character.birth_year}</td>
+              <td>{character.gender}</td>
+              <td>{character.homeworld}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   )
 }
